a6517820152fdcd33592b67a9b6446b4da1076df
Matthew Horan <matt@matthoran.com>
2010-08-02 23:50:09 -0400

    Cleanup; add intent-filter for TheirContacts activity.

--- src/com/matthoran/operator/MyContacts.java
+++ src/com/matthoran/operator/MyContacts.java
@@ -2,17 +2,11 @@ package com.matthoran.operator;
 
 import android.app.Activity;
 import android.os.Bundle;
-
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.AdapterView;
