b404b6bf5f5df8541a07e8de87c611d3850955d6
Matthew Horan <matt@matthoran.com>
2010-08-04 23:56:42 -0400

    TheirContacts shows the selected contact name!

--- src/com/matthoran/operator/TheirContacts.java
+++ src/com/matthoran/operator/TheirContacts.java
@@ -4,13 +4,37 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.util.Log;
+import android.provider.ContactsContract;
+import android.net.Uri;
+import android.database.Cursor;
+import android.widget.TextView;
 
 public class TheirContacts extends Activity {
+    private static final String TAG = "MyContacts";
+
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.their_contacts);
 
     Intent intent = getIntent();
-    Log.d("TheirContacts", intent.getDataString());
+    Log.d(TAG, intent.getDataString());
+    Cursor cursor = getContact(intent.getData());
+    TextView tv = (TextView) findViewById(R.id.theirContactsText);
+    if (cursor.moveToFirst()) {
+      String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
+      tv.setText(name);
+    }
   }
+
+    private Cursor getContact(Uri uri)
+    { 
+      String[] projection = new String[] {
+        ContactsContract.Contacts._ID,
+        ContactsContract.Contacts.DISPLAY_NAME,
+        ContactsContract.Contacts.LOOKUP_KEY
+      };
+
+      return managedQuery(uri, projection, null, null, null);
+    }
+
 }
